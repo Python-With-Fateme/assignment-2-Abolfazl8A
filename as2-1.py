b498092106033244b641278a094da299ff32bd0a
Your Name <abwlfdlbrwmnd2@gmail.com>
2022-09-03 07:49:01 -0700

    mse

---
+++ as2-1.py
@@ -0,0 +1,20 @@
+a = float ( input ("nomre 1 : ") )
+
+b = float ( input ("nomre 2 : ") )
+
+c = float ( input ("nomre 3 : ") )
+
+d = float ( input ("nomre 4 : ") )
+
+e = float ( input ("nomre 5 : ") )
+
+moadel= a+b+c+d+e
+
+kol= moadel/5
+
+if kol>17:
+    print('mmomtaz')
+elif 17>kol>12:
+    print('normal')
+elif 12>0:
+    print('mashrot')
---
+++ as2-2.py
@@ -0,0 +1,16 @@
+nu1=float(input ('adad : '))
+o=input('amal gar ryazl : ')
+
+if o=='+' or o=='-' or o=='*' or o=='/' or o=='**':
+    nu2=float(input('adad2 : '))
+
+if o=='+':
+    print (nu1+nu2)
+if o=='-':
+    print (nu1-nu2)
+if o=='*':
+    print (nu1*nu2)   
+if o=='/':
+    print (nu1/nu2)
+if o=='**':
+    print (nu1**nu2)
